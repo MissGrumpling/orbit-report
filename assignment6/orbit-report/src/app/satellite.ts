123f6944faf2606bd50bd94d67f5699a31314981
Sarah <sjermel425@gmail.com>
2021-04-05 18:56:40 -0500

    warning color

--- assignment6/orbit-report/src/app/satellite.ts
+++ assignment6/orbit-report/src/app/satellite.ts
@@ -11,6 +11,13 @@ export class Satellite {
        this.launchDate = launchDate;
        this.orbitType = orbitType;
        this.operational = operational;
+    }
 
+    shouldShowWarning(): boolean{
+        if(this.type.toLowerCase() === ('Space Debris').toLowerCase()){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
